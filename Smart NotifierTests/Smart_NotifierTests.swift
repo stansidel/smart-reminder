5d636603d411767f206b5bfe44b7b88c19ffddb6
Stan Sidel <sidelnikov.stanislav@gmail.com>
2017-09-08 09:09:07 +0500

    Initial Commit

---
+++ Smart NotifierTests/Smart_NotifierTests.swift
@@ -0,0 +1,36 @@
+//
+//  Smart_NotifierTests.swift
+//  Smart NotifierTests
+//
+//  Created by Stanislav Sidelnikov on 9/8/17.
+//  Copyright © 2017 Stanislav Sidelnikov. All rights reserved.
+//
+
+import XCTest
+@testable import Smart_Notifier
+
+class Smart_NotifierTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
